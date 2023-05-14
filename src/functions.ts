1d52eab61bcb585f6490dd22295c41556dd73733
Nitin Ganwani <nitganwani@gmail.com>
2023-05-14 12:26:51 +0200

    Add initial css styles

--- src/functions.ts
+++ src/functions.ts
@@ -20,23 +20,3 @@ export const createBoard = (cols: number, rows: number) => {
 
   return printValue;
 };
-
-let cols: number;
-let rows: number;
-
-const checkNeighbors = (x: number, y: number) => {
-  let count = 0;
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      const col = (x + i + cols) % cols;
-      const row = (y + j + rows) % rows;
-      count += board[col][row];
-    }
-  }
-
-  count -= board[x][y];
-  return count;
-};
-
-createBoard(5, 5);
-console.log(checkNeighbors(5, 5));
